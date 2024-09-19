5af5e035c7ef240f171dc517821cee6c9d2114e2
Thurain21 <thurainkosg@gmail.com>
2024-09-19 18:50:28 +0800

    creating project files

---
+++ src/main/java/com/sample/registration_project/RegistrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sample.registration_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationProjectApplication.class, args);
+	}
+
+}
