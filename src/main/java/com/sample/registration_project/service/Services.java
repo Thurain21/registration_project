94625ed6a3677322041d5ee1270171329a4b3fb2
Thurain21 <thurainkosg@gmail.com>
2024-09-21 18:30:57 +0800

    add update sevices

--- src/main/java/com/sample/registration_project/service/Services.java
+++ src/main/java/com/sample/registration_project/service/Services.java
@@ -8,4 +8,6 @@ public interface Services {
 
 	public ArrayList<userModel> register(userModel um);
 	public ArrayList<userModel> delete(String id);
+	public userModel updatedata(String id);
+	public ArrayList<userModel> datachange(userModel um);
 }
