7043318fd479cd59f41d909400abebf8393fb042
Thurain21 <thurainkosg@gmail.com>
2024-09-21 18:34:14 +0800

    modified services

--- src/main/java/com/sample/registration_project/service/serviceimpl.java
+++ src/main/java/com/sample/registration_project/service/serviceimpl.java
@@ -13,7 +13,7 @@ public class serviceimpl implements Services{
 	
 	@Override
 	public ArrayList<userModel> register(userModel um) {
-		um.setId(String.valueOf(datalist.size()+1));
+		um.setId(String.valueOf(datalist.size()));
 		datalist.add(um);
 		return datalist;
 	}
@@ -21,6 +21,18 @@ public class serviceimpl implements Services{
 	@Override
 	public ArrayList<userModel> delete(String id) {
 		datalist.remove(Integer.parseInt(id));
+		return datalist;
+	}
+
+	@Override
+	public userModel updatedata(String id) {
+		userModel um = datalist.get(Integer.parseInt(id));
+		return um;
+	}
+
+	@Override
+	public ArrayList<userModel> datachange(userModel um) {
+		datalist.set(Integer.parseInt(um.getId()), um);
 		return datalist;
 	}
 
