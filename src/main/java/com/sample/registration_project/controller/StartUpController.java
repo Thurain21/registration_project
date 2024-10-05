a14b66acde06f2c177dd1d9260414b67c131798f
Thurain21 <thurainkosg@gmail.com>
2024-10-05 23:28:20 +0800

    modified controller

--- src/main/java/com/sample/registration_project/controller/StartUpController.java
+++ src/main/java/com/sample/registration_project/controller/StartUpController.java
@@ -1,20 +1,27 @@
 package com.sample.registration_project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sample.registration_project.model.userModel;
+import com.sample.registration_project.service.serviceimpl;
 
 @Controller
 public class StartUpController {
 
+	@Autowired
+	serviceimpl sip ;
+	
 	@RequestMapping(value="/registration",method=RequestMethod.GET)
 	public ModelAndView register() {
-		userModel um = new userModel(null,null,0,null,null);
+		List<userModel> datalist  = sip.selectData();
 		ModelAndView model = new ModelAndView();
-		model.addObject(um);
+		model.addObject("datalist",datalist);
 		model.setViewName("registration");
 		return model;
 	}
