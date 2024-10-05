9af0e209678235fef4acd8d1ac20fffe17d9c71d
Thurain21 <thurainkosg@gmail.com>
2024-10-05 23:29:46 +0800

    modified repo

--- src/main/java/com/sample/registration_project/repository/RegistrationRepository.java
+++ src/main/java/com/sample/registration_project/repository/RegistrationRepository.java
@@ -1,34 +1,49 @@
 package com.sample.registration_project.repository;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.sample.registration_project.model.userModel;
 
 @Repository
 public class RegistrationRepository {
-	
+
 	@Autowired
 	private JdbcTemplate temp;
-	
-	private final String INSERT_QUERY = "INSERT INTO public.students(name, age, gender, country)	VALUES (?, ?, ?, ?)";
-	
-	public int register(userModel ud) {
+
+	private static final String INSERT_QUERY = "INSERT INTO public.students(name, age, gender, country)	VALUES (?, ?, ?, ?)";
+	private static final String SELECT_ALL_QUERY = "SELECT name, age, gender, country, id FROM public.students";
+
+	public int register(userModel um) {
 		return temp.update(INSERT_QUERY, new PreparedStatementSetter() {
-			
+
 			@Override
 			public void setValues(PreparedStatement ps) throws SQLException {
-				ps.setString(1, ud.getName());
-				ps.setInt(2, ud.getAge());
-				ps.setString(3, ud.getGender());
-				ps.setString(4, ud.getCountry());
-				
+				ps.setString(1, um.getName());
+				ps.setInt(2, um.getAge());
+				ps.setString(3, um.getGender());
+				ps.setString(4, um.getCountry());
+
 			}
 		});
 	}
+
+	public List<userModel> selectData() {
+		return temp.query(SELECT_ALL_QUERY, new RowMapper<userModel>() {
+
+			@Override
+			public userModel mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new userModel (rs.getString("id"),rs.getString("name"),rs.getInt("age"),rs.getString("gender"), rs.getString("country"));
+			}
+		});
+	}
+
 }
